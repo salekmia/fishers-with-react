267867d4a82ce1ff3c6b17f562653435726a3172
Abu Salek <bhairabbari330@gmail.com>
2021-09-26 05:28:09 +0600

    more data created on face DB

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
                     <h1>Get Famouse <span className="text-secondary">Fisher</span> Man</h1>
                     <h4>Budget: <span className="fs-1">20</span> milions</h4>
                 </div>
-                <img style={{height: '160px'}} className="img-fluid" src="./players/header-img.jpg" alt="" />
+                <img style={{height: '160px'}} className="img-fluid" src="./fishers/header-img.jpg" alt="" />
             </div>
             <hr />
         </div>
